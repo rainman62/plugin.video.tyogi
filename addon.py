2207d728b765e99e8bae115181cd7c0fc19cc175
rainman62 <el6291@gmail.com>
2018-09-25 00:25:51 -0500

    v0.0.2 Sub menues and Movie List

--- addon.py
+++ addon.py
@@ -5,20 +5,30 @@ import xbmcgui
 import xbmcplugin
 import xbmc
 
-import ListMenu
+import requests
+import re
 
-SERVER_BASE_URL = "http://tamilyogi.cc"
-CATEGORIES = {'home': "/home/",
-            'new':"/category/tamilyogi-full-movie-online/",
-            'brrip':"/category/tamilyogi-bluray-movies/",
-            'dvd':"/category/tamilyogi-dvdrip-movies/"}
+def getMoviesFromPage(url):
+    inputHTML = requests.get(url).text
+    inputList = inputHTML.split('</li>')
+    pattern = '<li.*href="(.*)".*title="(.*)".*src="(.*.jpg)".*'
+    returnValue = []
+    for input in inputList:
+        result = re.search(pattern, input, re.IGNORECASE | re.DOTALL)
+        if result is not None:
+            returnValue.append({'link': result.group(1), 'title': result.group(2), 'image': result.group(3)})
+    return returnValue
+
+SERVER_BASE_URL = "http://tamilyogi.cc/"
+CATEGORIES = {'home': "home/",
+            'new':"category/tamilyogi-full-movie-online/",
+            'brrip':"category/tamilyogi-bluray-movies/",
+            'dvd':"category/tamilyogi-dvdrip-movies/"}
 
 base_url = sys.argv[0]
 addon_handle = int(sys.argv[1])
 args = urlparse.parse_qs(sys.argv[2][1:])
 
-xbmc.log(sys.argv[2], level=xbmc.LOGNOTICE)
-xbmc.log(sys.argv[2][1:], level=xbmc.LOGNOTICE)
 
 xbmcplugin.setContent(addon_handle, 'movies')
 
@@ -26,24 +36,40 @@ xbmcplugin.setContent(addon_handle, 'movies')
 def build_url(query):
     return base_url + '?' + urllib.urlencode(query)
 
-current_category = args.get('category',None)
+#
+def webBuildPageURL(category,pageNumber):
+    return SERVER_BASE_URL + CATEGORIES.get(category) + 'page/' + pageNumber
 
-if current_category is None:
+current_category = args.get('current_category',None)
+current_level = args.get('level', 'MainMenu')
+current_page = args.get('page', None)
+current_movie = args.get('movie', None)
+
+if current_level == 'MainMenu':
     for category in CATEGORIES.keys():
-        url = build_url({'current_category': CATEGORIES.get(category)})
+        url = build_url({'current_category': category, 'level': 'Folder'})
         li = xbmcgui.ListItem(category.capitalize())
         xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
     xbmcplugin.endOfDirectory(addon_handle)
-else:
-    current_page = args.get('page',1)
-    if current_category == 'home':
-        ListMenu.myfunction()
-    elif current_category == 'new':
-        ListMenu.myfunction()
-    elif current_category == 'brrip':
-        ListMenu.myfunction()
-    elif current_category == 'dvd':
-        ListMenu.myfunction()
+elif current_level[0] == 'Folder':
+    my_current_page = '1' if current_page is None else current_page[0]
+    webPageURL = webBuildPageURL(current_category[0], my_current_page)
+    movieList = getMoviesFromPage(webPageURL)
+
+    for movieItem in movieList:
+        url = build_url({'level': 'Movie', 'movieURL': movieItem.get('link')})
+        li = xbmcgui.ListItem(movieItem.get('title'))
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+
+    url = build_url({'current_category': current_category[0], 'level': 'Folder', 'page': str(int(my_current_page)+1)})
+    li = xbmcgui.ListItem('Next..')
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+
+    xbmcplugin.endOfDirectory(addon_handle)
+elif current_level[0] == 'Movie':
+    xbmc.log("------------------ Inside Movie -------------", level=xbmc.LOGNOTICE)
+    xbmcplugin.endOfDirectory(addon_handle)
+
 # TODO : Render Main Menu from categories
 
 # TODO : Render List of movies from current categoryd
