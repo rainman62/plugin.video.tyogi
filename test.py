8381b5c566402e115a3eabdc718b38180d16664d
rainman62 <el6291@gmail.com>
2019-02-01 15:07:54 -0600

    Reverted to v0.0.2

--- test.py
+++ test.py
@@ -5,18 +5,9 @@ with open ("hello.txt", "r") as myfile:
     data=myfile.read()
 
 inputHTML = str(data)
-inputHTML = inputHTML[inputHTML.find('id=archive'):]
+inputHTML = inputHTML[inputHTML.find('id="content"'):]
 returnValue = {}
 # returnValue.append({'link': link, 'title': title, 'image': image})
-
-counter = 0
-
-def getCount():
-    return counter
-def incrementCount():
-    global counter
-    counter = counter + 1
-returnValueArray = []
 
 
 from HTMLParser import HTMLParser
@@ -26,16 +17,12 @@ class MyHTMLParser(HTMLParser):
         if tag == 'a' and len(attrs)>1 and attrs[0][0]== 'href' and attrs[1][0]=='title':
             print "Encountered a start tag:", tag, " attribute :", attrs
             if returnValue.get(attrs[1][1]) is None:
-                returnValue[attrs[1][1]] = getCount()
-                incrementCount()
-                returnValueArray.append({})
-            returnValueArray[returnValue[attrs[1][1]]]['link'] = attrs[0][1]
-            returnValueArray[returnValue[attrs[1][1]]]['title'] = attrs[1][1]
+                returnValue[attrs[1][1]] = {}
+            returnValue[attrs[1][1]]['link'] = attrs[0][1]
         elif tag == 'img' and len(attrs)>1 and attrs[0][0]== 'src' and attrs[1][0]=='alt':
             print "Encountered a start tag:", tag, " attribute :", attrs
-            returnValueArray[returnValue[attrs[1][1]]]['image'] = attrs[0][1]
-
+            returnValue[attrs[1][1]]['image'] = attrs[0][1]
 parser = MyHTMLParser()
 parser.feed(inputHTML)
 
-print returnValueArray
+print returnValue
